c64aeef3e7e1fc6463da7f7dc36244d4bad99e6b
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-18 12:43:11 +0300

    working files

--- drf-todolist-app/authentications/views.py
+++ drf-todolist-app/authentications/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_sites
 
 # Create your views here.
 
@@ -14,10 +17,18 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         user_data = serializer.data
+        user=User.objects.get(email=user_data['email'])
+
+        token=RefreshToken.for_user(user)
+        current_sites=get_current_sites(request)
+
+        data={'domain':current_sites.domain}
+        Util.send_email(data)
 
         return Response(user_data, status=status.HTTP_201_CREATED)
-        # user = User.objects.get(email=user_data['email'])
-        # token = RefreshToken
+    
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
 
