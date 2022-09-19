9052be69562378bcee72e536ee6b948e573cc3b5
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-19 14:52:22 +0300

    trying to solve the simple jwt

--- drf-todolist-app/todolist/settings.py
+++ drf-todolist-app/todolist/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    #'rest_framework_simplejwt.token',
     'drf_yasg',
     'authentications',
     'todos',
