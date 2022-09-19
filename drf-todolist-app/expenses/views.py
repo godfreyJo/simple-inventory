cff4519a8345dc52099212969ee559617168096e
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-19 16:46:50 +0300

    views for the expense module

--- drf-todolist-app/expenses/views.py
+++ drf-todolist-app/expenses/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import ExpenseSerializer
+from.models import Expense
+from rest_framework import permissions
+from .permissions import IsOwner
 
 
 # Create your views here.
 
 
-class ExpenseList(ListCreateAPIView):
+class ExpenseListAPIView(ListCreateAPIView):
     serializer_class = ExpenseSerializer
+    queryset = Expense.objects.all()
+    permissions = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        return self.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+
+class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ExpenseSerializer
+    queryset = Expense.objects.all()
+    permissions = (permissions.IsAuthenticated, IsOwner,)
+    lookup_field="id"
+
+    def perform_create(self, serializer):
+        return self.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
