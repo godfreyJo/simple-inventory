26f875133ecda83b778c338d64ef46381b0bd524
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-19 10:06:12 +0300

    logic for VerifyEmail & Token processing

--- drf-todolist-app/authentications/views.py
+++ drf-todolist-app/authentications/views.py
@@ -4,7 +4,11 @@ from .serializers import RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from .utils import Util
-from django.contrib.sites.shortcuts import get_current_sites
+from .models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+import jwt
+from django.conf import settings
 
 # Create your views here.
 
@@ -20,15 +24,31 @@ class RegisterView(generics.GenericAPIView):
         user_data = serializer.data
         user=User.objects.get(email=user_data['email'])
 
-        token=RefreshToken.for_user(user)
-        current_sites=get_current_sites(request)
+        token=RefreshToken.for_user(user).access_token
 
-        data={'domain':current_sites.domain}
+        current_site=get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi ' +user.username+' Use the link below to verify your email address\n' + absurl
+        data={'email_body': email_body, 'to_email': user.email, 'email_subject':'verify your email address'}
         Util.send_email(data)
 
         return Response(user_data, status=status.HTTP_201_CREATED)
     
 class VerifyEmail(generics.GenericAPIView):
-    def get(self):
-        pass
+    def get(self, request):
+        token=request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email':'Successfully activated account'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error':'Activation link expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error':'Invalid token'}, status=status.HTTP_HTTP_400_OK)
+            
 
