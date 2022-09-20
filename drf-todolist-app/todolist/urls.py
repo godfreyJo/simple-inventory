f4c077eabeaaaa98d4ae3a0446df76c7c28adb6f
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-20 12:46:07 +0300

    adding the expense route

--- drf-todolist-app/todolist/urls.py
+++ drf-todolist-app/todolist/urls.py
@@ -37,7 +37,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentications.urls')),    
+    path('auth/', include('authentications.urls')),   
+    path('expenses/', include('expenses.urls')), 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
