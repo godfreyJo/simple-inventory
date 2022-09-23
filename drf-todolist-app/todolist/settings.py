c4f172b1c7b30cd75e0dbaac512bc3debd1b05fb
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-23 10:20:49 +0300

    adding the REst settings

--- drf-todolist-app/todolist/settings.py
+++ drf-todolist-app/todolist/settings.py
@@ -98,6 +98,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
