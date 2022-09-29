766a15b6119c92c4694b18e7ade435689881770d
Godfrey Oyugi <godfreyoyugi@gmail.com>
2022-09-29 12:26:59 +0300

    adding the rqst reset passwd paths

--- drf-todolist-app/authentications/urls.py
+++ drf-todolist-app/authentications/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginAPIView
+from .views import RegisterView, VerifyEmail, LoginAPIView, PasswordTokenCheckAPI, RequestPasswordResetEmail
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
-    path('email-verify', VerifyEmail.as_view(), name='email-verify')
+    path('email-verify', VerifyEmail.as_view(), name='email-verify'),
+    # path('token/refresh', RefreshToken.as_view(), name='token_refresh'),
+    path('request-reset-email', RequestPasswordResetEmail.as_view(), name='request-reset-email'),
+    path('password-reset/<uid64>/<token>/', PasswordTokenCheckAPI.as_view(), name='password-reset-confirm')
 
 ]
